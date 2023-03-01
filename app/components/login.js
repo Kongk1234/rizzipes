94c0eb3ddb461d3d05bb6a3e170658bb16415d35
Alexander Olsen <alex191a@gmail.com>
2023-03-01 10:31:11 +0100

    login here

--- app/components/login.js
+++ app/components/login.js
@@ -1,9 +1,10 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class FormComponent extends Component
 {
-     url = '';
+    url = '';
     @tracked Username='';
     @tracked Password='';
 
