1d29a8fe250263243e1655ba3475d94b3872d79a
Alexander Olsen <alex191a@gmail.com>
2023-03-01 14:05:39 +0100

    login progress

--- app/components/login.js
+++ app/components/login.js
@@ -1,27 +1,34 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-
+import Route from '@ember/routing/route';
+import {inject} from '@ember/service';
 export default class FormComponent extends Component
 {
-    url = '';
+    url = 'https://api.theredwiking.com/auth/login';
     @tracked Username='';
     @tracked Password='';
+    @tracked Email ='';
+   
 
 
     @action async onSubmit(){
         const data = {
-            email: this.Username,
-            password: this.Password
+            username: this.Username,
+            password: this.Password,
+            email: this.Email
         }
-        const response = await fetch (url,{
+        const response = await fetch (this.url,{
             method:'POST',
             headers:{'Content-Type':'application/json'},
             body: JSON.stringify(data),
         });
         if(response.ok){
+            console.log("yes")
+            inject().transitionTo('Index')
             //route to homepage?
         }else{
+            console.log("no")
             //create error message and route user back to login
         }
     }
